40b69b603750868f2d334a2171837cab444f1f78
Angelo Paulo Lapus <angelo.paulo.a.lapus@accenture.com>
2024-01-04 13:48:03 +0800

    address review points

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit{
   }
 
   readmore(){
-    this.router.navigateByUrl('/services');
+    this.router.navigateByUrl('/about-us');
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
